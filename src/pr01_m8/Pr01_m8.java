9087d1e3c94978cfd98ec0f217cfa5b147f2a355
Roger <RogerLera93@gmail.com>
2016-03-07 16:16:02 +0100

    Versió 0.1 del projecte pr01_m8

---
+++ src/pr01_m8/Pr01_m8.java
@@ -0,0 +1,41 @@
+package pr01_m8;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Roger Lera
+ */
+public class Pr01_m8 {
+
+    public static void main(String[] args) {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write(" <head>");
+            bw.newLine();
+            bw.write(" <title>");
+            bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write(" </title>");
+            bw.newLine();
+            bw.write(" </head>");
+            bw.newLine();
+            bw.write(" <body>");
+            bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write(" </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Pr01_m8.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
