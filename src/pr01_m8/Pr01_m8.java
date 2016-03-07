d385485013c98b781f559a189c08b203df095317
RogerLera <RogerLera93@gmail.com>
2016-03-07 16:54:20 +0100

    Versió 0.2 del projecte pr01_m8

--- src/pr01_m8/Pr01_m8.java
+++ src/pr01_m8/Pr01_m8.java
@@ -27,6 +27,8 @@ public class Pr01_m8 {
             bw.newLine();
             bw.write(" <body>");
             bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4pr2</h1>");
+            bw.newLine();
             bw.write(" Nova p&agrave;gina web");
             bw.newLine();
             bw.write(" </body>");
